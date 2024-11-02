a32954e841bf66819b5e584831417451f342b819
dusk <y.bera003.06@protonmail.com>
2024-11-02 19:00:05 +0900

    feat: update arts

--- utils/filter-dead-twitter-links.sh
+++ utils/filter-dead-twitter-links.sh
@@ -3,9 +3,13 @@
 touch filtered_arts.txt
 while IFS= read -r line
 do
-    header=$(curl -o save -w "%{header_json}" "$line" 2> /dev/null)
-    echo $header | jq -e '.location[]' >/dev/null
-    if [ "$?" = "0" ]; then
+    if [[ $line == *"twitter.com"* ]]; then
+        header=$(curl -o save -w "%{header_json}" "$line" 2> /dev/null)
+        echo $header | jq -e '.location[]' >/dev/null
+        if [ "$?" = "0" ]; then
+            echo "$line" >> filtered_arts.txt
+        fi
+    else
         echo "$line" >> filtered_arts.txt
     fi
 done < $1
